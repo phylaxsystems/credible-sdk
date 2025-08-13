981ce3e945d0c80283c427accd92cdeb57b363d8
makemake <vukasin@gostovic.me>
2025-08-13 14:50:39 +0200

    feat: engine init

--- crates/sidecar/src/engine/mod.rs
+++ crates/sidecar/src/engine/mod.rs
@@ -39,3 +39,93 @@
 //! assertion reverts before approving a transaction.
 
 pub mod queue;
+
+use super::engine::queue::TransactionQueueReceiver;
+
+use assertion_executor::{
+    AssertionExecutor,
+    db::overlay::OverlayDb,
+    store::{
+        AssertionState,
+        AssertionStoreError,
+    },
+};
+use revm::{
+    DatabaseRef,
+    context::BlockEnv,
+    primitives::Address,
+};
+
+#[derive(thiserror::Error, Debug)]
+pub enum EngineError {
+    #[error("Database error")]
+    DatabaseError,
+    #[error("Transaction error")]
+    TransactionError,
+    #[error("Assertion error")]
+    AssertionError,
+}
+
+/// The engine processes blocks and appends transactions to them.
+#[derive(Debug, Clone)]
+pub struct CoreEngine<DB> {
+    state: OverlayDb<DB>,
+    tx_receiver: TransactionQueueReceiver,
+    assertion_executor: AssertionExecutor,
+    block_env: Option<BlockEnv>,
+}
+
+impl<DB: DatabaseRef> CoreEngine<DB> {
+    pub fn new(
+        state: OverlayDb<DB>,
+        tx_receiver: TransactionQueueReceiver,
+        assertion_executor: AssertionExecutor,
+    ) -> Self {
+        Self {
+            state,
+            tx_receiver,
+            assertion_executor,
+            block_env: None,
+        }
+    }
+
+    /// Creates a new `CoreEngine` for testing purposes.
+    /// Not to be used for anything but tests.
+    #[cfg(test)]
+    #[allow(dead_code)]
+    pub fn new_test() -> Self {
+        use assertion_executor::{
+            ExecutorConfig,
+            store::AssertionStore,
+        };
+
+        let (_, tx_receiver) = crossbeam::channel::unbounded();
+        Self {
+            state: OverlayDb::new(None, 64),
+            tx_receiver,
+            assertion_executor: AssertionExecutor::new(
+                ExecutorConfig::default(),
+                AssertionStore::new_ephemeral().expect("REASON"),
+            ),
+            block_env: None,
+        }
+    }
+
+    /// Inserts an assertion directly into the assertion store of the engine.
+    pub fn insert_into_store(
+        &self,
+        address: Address,
+        assertion: AssertionState,
+    ) -> Result<(), AssertionStoreError> {
+        self.assertion_executor
+            .store
+            .insert(address, assertion)
+            .map(|_| ())
+    }
+
+    /// Run the engine and process transactions and blocks received
+    /// via the transaction queue.
+    pub async fn run(mut self) -> Result<(), EngineError> {
+        unimplemented!()
+    }
+}
--- crates/sidecar/src/main.rs
+++ crates/sidecar/src/main.rs
@@ -1,9 +1,25 @@
-use clap::Parser;
-use rust_tracing::trace;
-
 mod args;
 mod engine;
 mod rpc;
+
+use assertion_executor::{
+    AssertionExecutor,
+    ExecutorConfig,
+    db::overlay::OverlayDb,
+    store::AssertionStore,
+};
+use crossbeam::channel::unbounded;
+use engine::CoreEngine;
+use std::convert::Infallible;
+
+use revm::database::{
+    CacheDB,
+    EmptyDBTyped,
+};
+
+use clap::Parser;
+use rust_tracing::trace;
+
 use args::SidecarArgs;
 
 #[tokio::main]
@@ -12,12 +28,25 @@ async fn main() -> anyhow::Result<()> {
 
     let args = SidecarArgs::parse();
 
+    let (_, tx_receiver) = unbounded();
+    let state: OverlayDb<CacheDB<EmptyDBTyped<Infallible>>> =
+        OverlayDb::new(None, 128 * 1024 * 1024);
+    let assertion_executor = AssertionExecutor::new(
+        ExecutorConfig::default(),
+        AssertionStore::new_ephemeral().expect("REASON"),
+    );
+
+    let engine = CoreEngine::new(state, tx_receiver, assertion_executor);
+
     tokio::select! {
         _ = tokio::signal::ctrl_c() => {
             println!("Received Ctrl+C, shutting down...");
         }
         _ = rpc::start_rpc_server(&args) => {
             println!("rpc server exited, shutting down...");
+        }
+        _ = engine.run() => {
+            println!("Engine run completed, shutting down...");
         }
     }
 
