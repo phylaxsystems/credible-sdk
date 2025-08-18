427487d964b74d715031c4143dcf80edb329d6ab
makemake <vukasin@gostovic.me>
2025-08-18 15:08:23 +0200

    fix: fmt

--- crates/sidecar/src/engine/queue.rs
+++ crates/sidecar/src/engine/queue.rs
@@ -8,16 +8,16 @@
 //! 2. Processed inside of transport and converted to `TxQueueContents`
 //! 3. Sent via the `TransactionQueueSender` to the engine
 //! 4. Received by engine and executed.
-//! 
+//!
 //! ## Channel contents
-//! 
+//!
 //! Channel contents can either be:
 //! - New blocks,
 //! - New transactions.
-//! 
+//!
 //! New block events contain `BlockEnv`s of the next block the sidcar should build on top of.
 //! At least one new block event is needed for the sidecar to accept transactions.
-//! 
+//!
 //! New transaction events are transactions that should be executed and included for the
 //! current `BlockEnv`.
 
