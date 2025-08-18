93aa47b5ab9f085c92fe533b7f793560f59ebb8a
makemake <vukasin@gostovic.me>
2025-08-18 14:55:50 +0200

    feat: docs/comments

--- crates/sidecar/src/transport/mod.rs
+++ crates/sidecar/src/transport/mod.rs
@@ -28,7 +28,11 @@ use crate::engine::queue::TransactionQueueSender;
 /// between the sidecar and driver (or sequencer). A transport should be entirely self contained
 /// spawning all services it needs to communicate inside of itself. The only way it should
 /// interact with the rest of the sidecar code is via the `TransactionQueueSender` passed to
-/// the driver on creation.
+/// the driver on creation. The transport will be spawned alongside other components of the
+/// sidecar and work alongside them.
+///
+/// The main transport task *should not* block. If the transport needs to block it is
+/// recommended to spawn a new blocking task for whatever action that blocks is needed.
 ///
 /// ## Associated Types
 ///
@@ -37,9 +41,32 @@ use crate::engine::queue::TransactionQueueSender;
 ///
 /// ## Lifecycle
 ///
-/// 1. **Creation**: Use `new()` with configuration and queue sender
-/// 2. **Execution**: Call `run()` to start the transport (typically in a background task)
+/// 1. **Creation**: Use `new()` with configuration and queue sender, called whenever a new transport is created (generally on startup)
+/// 2. **Execution**: Call `run()` to start the transport
 /// 3. **Shutdown**: Call `stop()` for graceful cleanup
+///
+/// ## Communicating with the driver
+///
+/// The transport should have 2-way communication with the driver:
+/// - to receive transactions,
+/// - to query for missing state.
+///
+/// The transport should have an endpoint to receive transactions/blockenvs from the driver
+/// it then converts to events it can send to the core engine.
+///
+/// If the core engine runs into state it does not have cached locally, it should query it via
+/// the transport, which should query the driver and store the response into the sidecar cache.
+///
+/// ### Querying and storing state; Communicating with engine
+///
+/// ***TODO: expand docs on interacting with state when implementing the state mod***
+///
+/// ## Erroring
+///
+/// The `Error` type defined in the trait should be used for exiting the transport when it
+/// encounters an error it cannot trivially recover from. The sidecar is designed to recover
+/// from individual component crashes, so if the transport exits with an error, it will attempt
+/// to restart it.
 #[allow(async_fn_in_trait)]
 pub trait Transport: Send + Sync {
     type Error: std::error::Error + Send;
