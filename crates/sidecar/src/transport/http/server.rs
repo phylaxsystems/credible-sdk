f343036dc0453d4ccc674ebc773845db1ce87056
makemake <vukasin@gostovic.me>
2025-08-20 14:25:42 +0200

    fix: skip error or result fields if not present

--- crates/sidecar/src/transport/http/server.rs
+++ crates/sidecar/src/transport/http/server.rs
@@ -61,8 +61,11 @@ pub struct JsonRpcRequest {
 #[derive(Debug, Serialize)]
 pub struct JsonRpcResponse {
     pub jsonrpc: String,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub result: Option<serde_json::Value>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub error: Option<JsonRpcError>,
+    #[serde(skip_serializing_if = "Option::is_none")]
     pub id: Option<serde_json::Value>,
 }
 
