c1c2ee2a172001df56c593fad2b75f2cb8e58140
makemake <vukasin@gostovic.me>
2025-09-08 17:48:46 +0200

    feat: add metrics to core engine

--- crates/sidecar/src/engine/mod.rs
+++ crates/sidecar/src/engine/mod.rs
@@ -45,7 +45,7 @@ use super::engine::queue::{
     TransactionQueueReceiver,
     TxQueueContents,
 };
-use crate::TransactionsState;
+use crate::{metrics::{BlockMetrics, TransactionMetrics}, TransactionsState};
 
 #[allow(unused_imports)]
 use assertion_executor::{
@@ -127,6 +127,7 @@ pub struct CoreEngine<DB> {
     assertion_executor: AssertionExecutor,
     block_env: Option<BlockEnv>,
     transaction_results: TransactionsResults,
+    block_metrics: BlockMetrics,
 }
 
 impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
@@ -149,6 +150,7 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
                 state_results,
                 transaction_results_max_capacity,
             ),
+            block_metrics:BlockMetrics::new(),
         }
     }
 
@@ -169,6 +171,7 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
             block_env: None,
             cache: Arc::new(Cache::new(vec![])),
             transaction_results: TransactionsResults::new(TransactionsState::new(), 10),
+            block_metrics:BlockMetrics::new(),
         }
     }
 
@@ -195,7 +198,10 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
         ),
         level = "debug"
     )]
-    fn execute_transaction(&mut self, tx_hash: B256, tx_env: &TxEnv) -> Result<(), EngineError> {
+    fn execute_transaction(&mut self, tx_hash: B256, tx_env: TxEnv) -> Result<(), EngineError> {
+        let mut tx_metrics = TransactionMetrics::new(tx_hash);
+        let instant = std::time::Instant::now();
+
         let mut fork_db = self.state.fork();
         let block_env = self.block_env.as_ref().ok_or_else(|| {
             error!("No block environment set for transaction execution");
@@ -217,12 +223,16 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
         }
 
         // Validate transaction and run assertions
-        let rax = match self.assertion_executor.validate_transaction_ext_db(
+        let rax = self.assertion_executor.validate_transaction_ext_db(
             block_env.clone(),
             tx_env.clone(),
             &mut fork_db,
             &mut self.state,
-        ) {
+        );
+
+        tx_metrics.transaction_processing_duration = instant.elapsed();
+
+        let rax = match rax {
             Ok(rax) => rax,
             Err(e) => {
                 match e {
@@ -263,6 +273,11 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
             }
         };
 
+        tx_metrics.assertions_per_transaction = rax.total_assertion_funcs_ran();
+        self.block_metrics.assertions_per_block += rax.total_assertion_funcs_ran();
+        tx_metrics.assertion_gas_per_transaction = rax.total_assertions_gas();
+        self.block_metrics.assertion_gas_per_block += rax.total_assertions_gas(); 
+
         let is_valid = rax.is_valid();
         let execution_result = rax.result_and_state.result.clone();
 
@@ -273,6 +288,8 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
             execution_result = ?execution_result,
             "Transaction processed"
         );
+
+        self.block_metrics.transactions_simulated_total += 1;
 
         if is_valid {
             // Transaction valid, passed assertions, commit state for successful transactions
@@ -295,7 +312,12 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
                     tx_hash = %tx_hash,
                     "Commiting state of successful tx"
                 );
+                self.block_metrics.block_gas_used += rax.result_and_state.result.gas_used();
+                self.block_metrics.transactions_simulated_success_total += 1;
+
                 self.state.commit(rax.result_and_state.state);
+            } else {
+                self.block_metrics.transactions_simulated_failure_total += 1;
             }
         } else {
             warn!(
@@ -310,6 +332,8 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
                 assertions_executions = ?rax.assertions_executions,
                 "Transaction validation details"
             );
+
+            self.block_metrics.invalidated_transactions_total += 1;
         }
 
         // Store the transaction result
@@ -378,6 +402,7 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
     pub async fn run(&mut self) -> Result<(), EngineError> {
         let mut processed_blocks = 0u64;
         let mut processed_txs = 0u64;
+        let mut block_processing_time = std::time::Instant::now();
 
         loop {
             // Use try_recv and yield when empty to be async-friendly
@@ -406,6 +431,7 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
                     debug!(
                         target = "engine",
                         timestamp = block_env.timestamp,
+                        number = block_env.number,
                         gas_limit = block_env.gas_limit,
                         base_fee = ?block_env.basefee,
                         "Block details"
@@ -418,12 +444,21 @@ impl<DB: DatabaseRef + Send + Sync> CoreEngine<DB> {
                     }
 
                     self.cache.set_block_number(block_env.number);
+
+                    self.block_metrics.block_processing_duration = block_processing_time.elapsed();
+                    self.block_metrics.commit();
+                    self.block_metrics = BlockMetrics::new();
+                    self.block_metrics.current_height = block_env.number;
+                    block_processing_time = std::time::Instant::now();
+
                     self.block_env = Some(block_env);
+
                 }
                 TxQueueContents::Tx(queue_transaction) => {
                     let tx_hash = queue_transaction.tx_hash;
                     let tx_env = queue_transaction.tx_env;
                     processed_txs += 1;
+                    self.block_metrics.transactions_considered_total += 1;
 
                     if self.block_env.is_none() {
                         error!(
--- crates/sidecar/src/metrics.rs
+++ crates/sidecar/src/metrics.rs
@@ -1,3 +1,4 @@
+use assertion_executor::primitives::FixedBytes;
 use metrics::{
     counter,
     gauge,
@@ -7,37 +8,40 @@ use metrics::{
 /// Individual block metrics we commit to the prometheus exporter.
 ///
 /// Will commit metrics when dropped.
-#[derive(Clone, Debug)]
+#[derive(Clone, Debug, Default)]
 pub struct BlockMetrics {
-    /// Duration we spent processing transactions
-    pub transaction_processing_duration: std::time::Duration,
     /// Entire duration from blockenv to blockenv
     pub block_processing_duration: std::time::Duration,
     /// How many transactions the engine has seen
-    pub transactions_considered_total: u64,
+    pub transactions_considered_total: u64, //
     /// How many txs were executed
-    pub transactions_simulated_total: u64,
+    pub transactions_simulated_total: u64, //
     /// How many transactions we have executed successfully
-    pub transactions_simulated_success_total: u64,
+    pub transactions_simulated_success_total: u64, //
     /// How many transactions we have executed unsuccessfully
-    pub transactions_simulated_failure_total: u64,
+    pub transactions_simulated_failure_total: u64, //
     /// How many transactions we have executed successfully,
     /// which ended up invalidating assertions
-    pub invalidated_transactions_total: u64,
+    pub invalidated_transactions_total: u64, //
     /// How much gas was used in a block
-    pub block_gas_used: u64,
+    pub block_gas_used: u64, //
     /// How many assertions we have executed in the block
-    pub assertions_per_block: u64,
+    pub assertions_per_block: u64, //
     /// How much assertion gas we executed in a block
-    pub assertion_gas_per_block: u64,
+    pub assertion_gas_per_block: u64, //
     /// Current block height
-    pub current_height: u64,
+    pub current_height: u64, //
 }
 
 impl BlockMetrics {
+    pub fn new() -> Self {
+        Self {
+            ..Default::default()
+        }
+    }
+
     /// Commits the metrics
     pub fn commit(&self) {
-        gauge!("transaction_processing_duration").set(self.transaction_processing_duration);
         gauge!("block_processing_duration").set(self.block_processing_duration);
         counter!("transactions_considered_total").increment(self.transactions_considered_total);
         counter!("transactions_simulated_total").increment(self.transactions_simulated_total);
@@ -62,21 +66,34 @@ impl Drop for BlockMetrics {
 /// Will commit metrics when dropped.
 #[derive(Clone, Debug)]
 pub struct TransactionMetrics {
-    pub hash: String,
+    pub hash: FixedBytes<32>,
     /// How much assertion gas a transaction spent
     pub assertion_gas_per_transaction: u64,
     /// How many assertions we have executed per transaction
     pub assertions_per_transaction: u64,
+    /// Duration we spent processing a transaction in microseconds
+    pub transaction_processing_duration: std::time::Duration,
     /// How much gas we have executed per assertion
     pub gas_per_assertion: u64,
 }
 
 impl TransactionMetrics {
+    pub fn new(hash: FixedBytes<32>) -> Self {
+        Self {
+            hash,
+            assertion_gas_per_transaction: 0,
+            assertions_per_transaction: 0,
+            transaction_processing_duration: std::time::Duration::default(),
+            gas_per_assertion: 0,
+        }
+    }
+
     /// Commits the per tx metrics
     pub fn commit(&self) {
-        histogram!("assertion_gas_per_transaction", "tx_hash" => self.hash.clone()).record(self.assertion_gas_per_transaction as f64);
-        histogram!("assertions_per_transaction", "tx_hash" => self.hash.clone()).record(self.assertions_per_transaction as f64);
-        histogram!("gas_per_assertion", "tx_hash" => self.hash.clone()).record(self.gas_per_assertion as f64);
+        histogram!("assertion_gas_per_transaction", "tx_hash" => self.hash.to_string()).record(self.assertion_gas_per_transaction as f64);
+        histogram!("assertions_per_transaction", "tx_hash" => self.hash.to_string()).record(self.assertions_per_transaction as f64);
+        histogram!("transaction_processing_duration", "tx_hash" => self.hash.to_string()).record(self.transaction_processing_duration);
+        histogram!("gas_per_assertion", "tx_hash" => self.hash.to_string()).record(self.gas_per_assertion as f64);
     }
 }
 
