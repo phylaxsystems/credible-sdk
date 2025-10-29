5ceee068bb35d5701df9b11bcb07e262e2d93013
Branimir Malesevic <glbranimir@gmail.com>
2025-10-29 11:34:11 +0100

    state-worker: add linea devnet (#237)

--- crates/state-worker/src/genesis_data/mod.rs
+++ crates/state-worker/src/genesis_data/mod.rs
@@ -1,3 +1,4 @@
+mod linea_devnet;
 mod linea_sepolia;
 
 use serde_json::Value;
@@ -6,6 +7,7 @@ use serde_json::Value;
 pub(crate) fn for_chain_id(chain_id: u64) -> Option<&'static Value> {
     match chain_id {
         linea_sepolia::CHAIN_ID => Some(&linea_sepolia::GENESIS),
+        linea_devnet::CHAIN_ID => Some(&linea_devnet::GENESIS),
         _ => None,
     }
 }
