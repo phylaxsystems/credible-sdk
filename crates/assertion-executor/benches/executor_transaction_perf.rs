c9a68f7170292f47a3b3f4a4cdf6305dd5211287
makemake <vukasin@gostovic.me>
2026-01-27 16:45:40 +0100

    perf: add erc20 bench

--- crates/assertion-executor/benches/executor_transaction_perf.rs
+++ crates/assertion-executor/benches/executor_transaction_perf.rs
@@ -47,6 +47,37 @@ fn executor_transaction_performance_benchmark(c: &mut Criterion) {
         );
     });
 
+    let single_erc20_tx = LoadDefinition {
+        tx_amount: 1,
+        eoa_percent: 0.0,
+        erc20_percent: 100.0,
+        uni_percent: 0.0,
+        aa_percent: 0.0,
+    };
+
+    let mut single_erc20_tx_aa = single_erc20_tx;
+    single_erc20_tx_aa.aa_percent = 100.0;
+
+    group.bench_function("erc20_transaction", |b| {
+        b.iter_batched(
+            || BenchmarkPackage::new(single_erc20_tx),
+            |mut package| {
+                package.run().expect("benchmark run failed");
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
+    group.bench_function("erc20_transaction_aa", |b| {
+        b.iter_batched(
+            || BenchmarkPackage::new(single_erc20_tx_aa),
+            |mut package| {
+                package.run().expect("benchmark run failed");
+            },
+            BatchSize::SmallInput,
+        );
+    });
+
     group.finish();
 }
 
